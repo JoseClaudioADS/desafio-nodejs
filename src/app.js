7d7d41bc5b6107e27158b249ca6305f2663b8b17
Jose Claudio <jcsj.ti@gmail.com>
2020-04-16 23:42:27 -0300

    Código do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const id = uuid();
+  const repository = { id, likes: 0, ...request.body };
+  repositories.push(repository);
+  response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((rep) => rep.id === id);
+
+  if (index < 0) {
+    return response.status(400).send();
+  }
+
+  const repository = { id, ...request.body, likes: 0 };
+  repositories[index] = repository;
+  response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((rep) => rep.id === id);
+
+  if (index < 0) {
+    return response.status(400).send();
+  }
+  repositories.splice(index, 1);
+  response.send(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((rep) => rep.id === id);
+
+  if (index < 0) {
+    return response.status(400).send();
+  }
+
+  const repository = repositories[index];
+  repository.likes++;
+
+  repositories[index] = repository;
+
+  response.send(repository);
 });
 
 module.exports = app;
